884de61dc6784a4b712dc024eb60c3de78ad476d
Justin Bartlett <justbartlett@gmail.com>
2020-04-19 13:00:52 -0400

    code splitting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense  } from "react";
 import ReactDOM from "react-dom";
 import { Router } from "@reach/router";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+// import Details from "./Details"; - we'll lazy load this
+// import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
+
+// details is placeholder component that will not load this code until Details is to be rendered for the first time
+// parcel splits this into a separate bundle
+const Details = lazy(() => import('./Details'));
+const SearchParams = lazy(() => import('./SearchParams'));
 
 const App = () => {
   const theme = useState("darkblue");
@@ -12,10 +17,13 @@ const App = () => {
     <ThemeContext.Provider value={theme}>
       <div>
         <NavBar />
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        {/* The fallback will display until the route is loaded. */}
+        <Suspense fallback={<h1>Loading route</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
