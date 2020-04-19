2693b1ee8693da5afe2e6213a61c01f54ed63fab
Justin Bartlett <justbartlett@gmail.com>
2020-04-19 18:42:10 -0400

    heterogeneous arrays and hooks

--- src/useDropdown.tsx
+++ src/useDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FunctionComponent } from "react";
+import React, { useState, FunctionComponent, Dispatch } from "react";
 
 const useDropdown = (label: string, defaultState: string, options: string[]) => {
   const [state, updateState] = useState(defaultState);
@@ -22,7 +22,7 @@ const useDropdown = (label: string, defaultState: string, options: string[]) => 
       </select>
     </label>
   );
-  return [state, Dropdown, updateState];
+  return [state, Dropdown, updateState] as [string, FunctionComponent, Dispatch<string>];
 };
 
 export default useDropdown;
